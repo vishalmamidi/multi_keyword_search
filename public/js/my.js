8c210e3d1793269c8a944c02e8c1a8859df3e582
vishalmamidi <mamidivishalsa@gmail.com>
2017-04-12 19:50:35 +0530

    text fixes products to posts

--- public/js/my.js
+++ public/js/my.js
@@ -3,7 +3,7 @@ new Vue({
     el: 'body',
 
     data: {
-	    products: [],
+	    posts: [],
 	    loading: false,
 	    error: false,
     	query: ''
@@ -13,15 +13,15 @@ new Vue({
 	    search: function() {
 	        // Clear the error message.
 	        this.error = '';
-	        // Empty the products array so we can fill it with the new products.
-	        this.products = [];
+	        // Empty the posts array so we can fill it with the new posts.
+	        this.posts = [];
 	        // Set the loading property to true, this will display the "Searching..." button.
 	        this.loading = true;
 
 	        // Making a get request to our API and passing the query to it.
 	        this.$http.get('/api/search?q=' + this.query).then((response) => {
-	            // If there was an error set the error message, if not fill the products array.
-	            response.body.error ? this.error = response.body.error : this.products = response.body;
+	            // If there was an error set the error message, if not fill the posts array.
+	            response.body.error ? this.error = response.body.error : this.posts = response.body;
 	            // The request is finished, change the loading to false again.
 	            this.loading = false;
 	            // Clear the query.
--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -61,20 +61,24 @@
             </div>
 
 
-            <div id="products" class="row list-group">
-            <div class="item col-xs-4 col-lg-4" v-for="product in products">
-                <div class="thumbnail ">
+            <div id="posts" class="row list-group">
+            <div class="item col-xs-4 col-lg-4" v-for="post in posts">
+                <div class="thumbnail">
                         
                         <div class="caption">
-                            <h4 class="group inner list-group-item-heading">@{{ product.title }}</h4>
-                            <p class="group inner list-group-item-text">@{{ product.description }}</p>
-                            <div class="row">
-                                <div class="col-xs-12 col-md-6">
-                                    <a class="btn btn-success" href="/posts/@{{ product.id }}">View Post</a>
-                                </div>
-                            </div>
+                            <h4 class="group inner list-group-item-heading"><b> @{{ post.title }} </b></h4>
+                            <p class="group inner list-group-item-text">@{{ post.description }}</p>
+                            
+                            <div class=" text-right">
+                                            
+                               <a class="btn btn-success " href="/posts/@{{ post.id }}">View Post</a> 
+
+                            </div> 
+                            
+                            
 
                         </div>
+
                     </div>
                 </div>
             </div>
